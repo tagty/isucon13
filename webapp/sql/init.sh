6858f7d207015658c8c92ba48da1b62eaa8a2d3f
tagty <bouzu.ao@gmail.com>
2024-11-01 23:59:19 +0900

    アプリケーションを2に移動 #63

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -68,6 +68,5 @@ mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < initial_livecomments.sql
 
-bash ../pdns/init_zone.sh 
-
-
+# bash ../pdns/init_zone.sh
+ssh 192.168.0.11 bash webapp/pdns/init_zone.sh
